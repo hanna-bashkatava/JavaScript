f78b95b01badf5906d43dc477db50d980768ca5d
HANNA BASHKATAVA <bashka0789@mail.ru>
2020-12-13 13:11:35 +0300

    yandex changes

--- yandex.js
+++ yandex.js
@@ -1,5 +1,6 @@
 describe('angularjs homepage', function () {
-    it('should greet the named user', function () {
+    it('should greet the named user',async function () {
+        await browser.waitForAngularEnabled(false);
         browser.get('http://www.yandex.com');
     });
 });
