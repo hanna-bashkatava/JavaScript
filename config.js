4b811ce5c2c424fb761c24c094d552848639508a
HANNA BASHKATAVA <bashka0789@mail.ru>
2020-12-16 17:05:01 +0300

    123

--- config.js
+++ config.js
@@ -11,11 +11,7 @@ exports.config = {
   
     // Spec patterns are relative to the current working directly when
     // protractor is called.
-<<<<<<< HEAD
     specs: ['sources/output_js/MyFirstTest.js'],
-=======
-    specs: ['MyFirstTest.js'],
->>>>>>> a626d43f924546090392db716c87ede1ad4513fd
   
     // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
