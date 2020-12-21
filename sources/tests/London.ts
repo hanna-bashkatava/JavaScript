c2aaf66622187f461033455ffbd5de9b0cd1b965
HANNA BASHKATAVA <bashka0789@mail.ru>
2020-12-21 14:33:39 +0300

    clear code in London file

--- sources/tests/London.ts
+++ sources/tests/London.ts
@@ -1,74 +1,47 @@
 import { browser, by, element, ElementFinder, promise, protractor} from "protractor";
 
+beforeAll (() =>      {
+    browser.waitForAngularEnabled(false);
+})
+
 describe('Search', () => {
     it('Open yandex', async () => {
         
-        let EC = protractor.ExpectedConditions;
-
-        await browser.waitForAngularEnabled(false);
+        const EC = protractor.ExpectedConditions;
 
         await browser.get('https://www.yandex.by/');
         
-        let geolinkButton = element(by.css(".geolink__reg")).click();
+        const geolinkButton = element(by.css(".geolink__reg"));
 
+        await geolinkButton.click();
         await browser.wait(EC.urlContains('yandex'), 10000);
 
-        let inputBox = element(by.css("#city__front-input"));
+        const inputBox = element(by.css("#city__front-input"));
 
         await browser.wait(EC.presenceOf(inputBox), 10000);
-
         await inputBox.clear();
-
         await inputBox.sendKeys("Лондон");
 
-        let selectButton = element(by.css('.b-autocomplete-item__reg'));
+        const selectButton = element(by.css('.b-autocomplete-item__reg'));
 
         await browser.wait(EC.presenceOf(selectButton), 10000);
-
         await selectButton.click();
                           
-        let moreButton = element(by.css("[data-statlog='services_new.more']"));
+        const moreButton = element(by.css("[data-statlog='services_new.more']"));
               
         await browser.wait(EC.presenceOf(moreButton), 10000);
-       
         await moreButton.click();
-
-        let content1 = element(by.css(".services-new__more-popup-content"));
-
-        content1.getText().then(cont => { console.log("text:", cont); })
-
-        let geolink_button_2 = element(by.css(".geolink__reg")).click();
-
-        let inputBox2 = element(by.css("#city__front-input"));
-                          
-        await browser.wait(EC.presenceOf(inputBox2), 10000);
-              
-        await inputBox2.clear();
-
-        await inputBox2.sendKeys("Париж");
-
-        let selectButton2 = element(by.css('.b-autocomplete-item__reg'));
-        
-        await browser.wait(EC.presenceOf(selectButton2), 10000);
-
-        await selectButton2.click();
-
-        let moreButton2 = element(by.css("[data-statlog='services_new.more']"));
-              
-        await browser.wait(EC.presenceOf(moreButton2), 10000);
-       
-        await moreButton2.click();
-               
-        let content2 = element(by.css(".services-new__more-popup-content"));
-
-        await browser.wait(EC.presenceOf(content2), 5000);
-
-        await content2.getText().then(cont2 => { console.log("text2:", cont2);
-
-        expect(content2.getText()).toEqual(content1.getText()); 
-        
-        })
-
+        let contentLondon = element.all(by.css(".services-new__more-popup-content"));
+        await geolinkButton.click();                         
+        await browser.wait(EC.presenceOf(inputBox), 10000);
+        await inputBox.clear();  
+        await inputBox.sendKeys("Париж");               
+        await browser.wait(EC.presenceOf(selectButton), 10000);
+        await selectButton.click();                      
+        await browser.wait(EC.presenceOf(moreButton), 10000);       
+        await moreButton.click();               
+        let contentParis = element.all(by.css(".services-new__more-popup-content"));        
+        expect(contentParis.getText()).toEqual(contentLondon.getText());                
         afterAll (() => {
         browser.close
         })
